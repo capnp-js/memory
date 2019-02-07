3b75349c9033363efc7fc807ed341e50e3c4c450
Tim Popham <popham@uw.edu>
2019-02-06 23:01:05 -0800

    Switch to Mocha and Istanbul

--- test/unit/assertInBounds.js
+++ test/unit/assertInBounds.js
@@ -1,31 +1,30 @@
 /* @flow */
 
-import test from "ava";
+import * as assert from "assert";
+import { describe, it } from "mocha";
 
 import { SegmentRangeError, assertInBounds } from "../../src/assertInBounds";
 
-const segment = {
-  id: 0,
-  raw: new Uint8Array(16),
-  end: 16,
-};
+const segment = { id: 0, raw: new Uint8Array(16), end: 16 };
 
-test("`assertInBounds`", t => {
-  t.plan(2);
-
-  t.notThrows(() => {
-    const left = {
-      segment,
-      position: 0,
-    };
-    assertInBounds(left, 16);
+describe("assertInBounds", function () {
+  it("doesn't throw for in bounds ranges", function () {
+    assert.doesNotThrow(() => {
+      const left = {
+        segment,
+        position: 0,
+      };
+      assertInBounds(left, 16);
+    });
   });
 
-  t.throws(() => {
-    const left = {
-      segment,
-      position: 8,
-    };
-    assertInBounds(left, 9);
-  }, SegmentRangeError);
+  it("throws for out of bounds ranges", function () {
+    assert.throws(() => {
+      const left = {
+        segment,
+        position: 8,
+      };
+      assertInBounds(left, 9);
+    }, SegmentRangeError);
+  });
 });
--- test/unit/isNull.js
+++ test/unit/isNull.js
@@ -1,39 +1,36 @@
 /* @flow */
 
-import test from "ava";
+import * as assert from "assert";
+import { describe, it } from "mocha";
 
 import isNull from "../../src/isNull";
 
 //TODO: I vaguely recall that some exports on capnp's index exist solely for testing.
 //      This seems to be working fine without the global export so consider removing some of the surface area on index.
-const segment = {
-  id: 0,
-  raw: new Uint8Array(72),
-  end: 72,
-};
+const segment = { id: 0, raw: new Uint8Array(72), end: 72 };
 
-segment.raw[0] = 1;
-segment.raw[9] = 1;
-segment.raw[18] = 1;
-segment.raw[27] = 1;
-segment.raw[36] = 1;
-segment.raw[45] = 1;
-segment.raw[54] = 1;
-segment.raw[63] = 1;
+describe("isNull", function () {
+  segment.raw[0] = 1;
+  segment.raw[9] = 1;
+  segment.raw[18] = 1;
+  segment.raw[27] = 1;
+  segment.raw[36] = 1;
+  segment.raw[45] = 1;
+  segment.raw[54] = 1;
+  segment.raw[63] = 1;
 
-test("`isNull` false", t => {
-  t.plan(8);
+  it("reports zero pointers as null", function () {
+    assert.ok(!isNull({segment, position: 0}));
+    assert.ok(!isNull({segment, position: 8}));
+    assert.ok(!isNull({segment, position: 16}));
+    assert.ok(!isNull({segment, position: 24}));
+    assert.ok(!isNull({segment, position: 32}));
+    assert.ok(!isNull({segment, position: 40}));
+    assert.ok(!isNull({segment, position: 48}));
+    assert.ok(!isNull({segment, position: 56}));
+  });
 
-  t.false(isNull({segment, position: 0}));
-  t.false(isNull({segment, position: 8}));
-  t.false(isNull({segment, position: 16}));
-  t.false(isNull({segment, position: 24}));
-  t.false(isNull({segment, position: 32}));
-  t.false(isNull({segment, position: 40}));
-  t.false(isNull({segment, position: 48}));
-  t.false(isNull({segment, position: 56}));
-});
-
-test("`isNull` true", t => {
-  t.true(isNull({segment, position: 64}));
+  it("reports non-zero pointers as non-null", function () {
+    assert.ok(isNull({segment, position: 64}));
+  });
 });
